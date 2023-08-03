788b9d90a452a3ebf963423138f7a9d14d95ebb3
Juan Ramírez <95980672+juangramirez@users.noreply.github.com>
2023-08-03 18:46:26 -0300

    Agregando permisos
    Solo la configuración

--- Bluetree/app/src/main/java/com/bluetree/bluetree/MainActivity.java
+++ Bluetree/app/src/main/java/com/bluetree/bluetree/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bluetree.bluetree;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.net.Uri;
-import android.provider.DocumentsContract;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 1;
     Button actualizar;
+    Button permitirBlu;
+
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
 
         actualizar = findViewById(R.id.actualizar);
 
+        permitirBlu = findViewById(R.id.permitirBlu); //Lo primero es el nombre de la variable de tipo botón, el segundo permitirBlu es el id en ui
+
+        //Si getDefaultAdapter() devuelve null, significa que el dispositivo no es compatible con Bluetooth.
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+    }
+
+    public void permitirBluetooth(View view){
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
 
     public void actualizarAccion(View view){
